81d79e2760d4252b16656c0bd2ede2e639b38752
rrauf8178 <rrauf8178@gmail.com>
2022-11-25 22:59:48 +0400

    Adding image, Head component for title, meta tag, Data fetching getStaticPath()

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import Hello from "./Hello";
+import Image from "next/image";
 
 const Layout = ({ children }) => {
-  return <div className="container">
-  <Hello/>
-  {children}
-  </div>;
+  return (
+    <div className="container">
+      <Image src="/photo.jpg" width={300} height={200} alt="photo"/>
+      <Hello />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import Component from "../components/Component";
+import Head from "next/head";
 
 const index = () => {
   return (
     <div>
+    <Head>
+      <title>Home learn</title>
+      <meta name="keywords" content="Next.js, javascript, react ...."/>
+    </Head>
       <Link href="/about">
         <p>Go to about page</p>
       </Link>
